c440957a529fc7ad204e38be21e769af693350dd
qk <chiangqinkang@gmail.com>
2024-09-29 00:16:07 +0800

    Fix tests

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -17,11 +17,11 @@ import seedu.address.model.util.SampleDataUtil;
  */
 public class PersonBuilder {
 
-    public static final String DEFAULT_NAME = "Alice Pauline";
+    public static final String DEFAULT_NAME = "Amy Bee";
     public static final String DEFAULT_PHONE = "85355255";
-    public static final String DEFAULT_EMAIL = "alice@gmail.com";
+    public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_REMARK = "She likes aardvarks.";
+    public static final String DEFAULT_REMARK = "Likes to swim.";
 
     private Name name;
     private Phone phone;
@@ -31,7 +31,7 @@ public class PersonBuilder {
     private Set<Tag> tags;
 
     /**
-     * Constructs a Person with the given person details.
+     * Creates a {@code PersonBuilder} with the default details.
      */
     public PersonBuilder() {
         name = new Name(DEFAULT_NAME);
@@ -101,8 +101,6 @@ public class PersonBuilder {
         this.remark = new Remark(remark);
         return this;
     }
-
-
 
     public Person build() {
         return new Person(name, phone, email, address, remark, tags);
